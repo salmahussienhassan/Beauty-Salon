39f49e9eeadfc8984b8eb5a5609289f5bc71f470
salmahussien <salmahussien078@gmail.com>
2024-08-01 16:46:54 +0300

    Last Commit

--- Beauty/src/app/core/home/home.component.ts
+++ Beauty/src/app/core/home/home.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
