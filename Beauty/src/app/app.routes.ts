41a1f840515d605eeb9bfea9035024ce0e3e3d24
salmahussien <salmahussien078@gmail.com>
2024-08-01 15:06:12 +0300

    create home

--- Beauty/src/app/app.routes.ts
+++ Beauty/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './core/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'layout/home', pathMatch: "full" },
+    {path:'home',component:HomeComponent},
+];
