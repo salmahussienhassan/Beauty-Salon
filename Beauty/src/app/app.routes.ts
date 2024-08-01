0cc906775ce19ddd2ab6dffc13125b844e676167
salmahussien <salmahussien078@gmail.com>
2024-08-01 15:33:15 +0300

    Fix Import

--- Beauty/src/app/app.routes.ts
+++ Beauty/src/app/app.routes.ts
@@ -2,6 +2,6 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './core/home/home.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'layout/home', pathMatch: "full" },
+    { path: '', redirectTo: 'home', pathMatch: "full" },
     {path:'home',component:HomeComponent},
 ];
